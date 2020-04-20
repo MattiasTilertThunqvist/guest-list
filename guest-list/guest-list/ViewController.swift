fab172fcb6d8ac1c7a7aeb50aeb7b59069fbe2db
Mattias Tilert Thunqvist <mattias@tilert.com>
2020-04-20 14:39:34 +0200

    Create Xcode projects

---
+++ guest-list/guest-list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guest-list
+//
+//  Created by Mattias Tilert Thunqvist on 2020-04-20.
+//  Copyright © 2020 Mattias Tilert Thunqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
