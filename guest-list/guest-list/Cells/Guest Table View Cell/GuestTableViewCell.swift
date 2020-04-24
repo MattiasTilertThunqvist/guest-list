c4b26c1be93d9d014f975ee70b462415aee269fc
Mattias Tilert Thunqvist <mattias@tilert.com>
2020-04-24 12:23:24 +0200

    - Setup cell

---
+++ guest-list/guest-list/Cells/Guest Table View Cell/GuestTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  GuestTableViewCell.swift
+//  guest-list
+//
+//  Created by Mattias Tilert Thunqvist on 2020-04-24.
+//  Copyright © 2020 Mattias Tilert Thunqvist. All rights reserved.
+//
+
+import UIKit
+
+class GuestTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    static let cellIdentifier = "GuestTableViewCell"
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var companyLabel: UILabel!
+    
+    // MARK: Init
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    // MARK: Accessors
+    
+    func setGuestName(to text: String) {
+        nameLabel.text = text
+    }
+    
+    func setCompanyLabel(to number: Int) {
+        companyLabel.text = number == 0 ? nil : "(+\(number))"
+    }
+}
--- guest-list/guest-list/Guest List/GuestListViewController.swift
+++ guest-list/guest-list/Guest List/GuestListViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class GuestListViewController: UIViewController {
+class GuestListViewController: UITableViewController {
+    
+    // MARK: Properties
+    
 
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        setupTableView()
     }
-
-
+    
+    private func setup() {
+        
+    }
+    
+    private func setupTableView() {
+        registerNibs()
+    }
+    
+    private func registerNibs() {
+        let cellNib = UINib(nibName: GuestTableViewCell.cellIdentifier, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: GuestTableViewCell.cellIdentifier)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GuestTableViewCell.cellIdentifier, for: indexPath) as! GuestTableViewCell
+        cell.setGuestName(to: "Mattias Tilert Thunqvist")
+        cell.setCompanyLabel(to: 1)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
 }
-
+ 
